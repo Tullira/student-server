21d36bb3e5142ca5a13ddbde75da1ff37edef7cb
titi-lima <titisau@gmail.com>
2023-08-23 23:51:47 -0300

    chore(request-handler-patch): added option to respond from controller

--- src/middlewares/requestHandler.ts
+++ src/middlewares/requestHandler.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from 'express';
 
 const requestHandler = (req: Request, res: Response) => {
-  res.status(res.locals.status).json({ data: res.locals.data, message: res.locals.message });
+  // se não houver status, a response já foi enviada e não precisa ser enviada uma segunda vez
+  if (!res.locals.status) return;
+  res
+    .status(res.locals.status)
+    .json({ data: res.locals.data, message: res.locals.message });
 };
 
 export default requestHandler;
