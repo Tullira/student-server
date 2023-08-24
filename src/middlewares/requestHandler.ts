62d4a80ab4a591789daab5e64e8e959033431591
titi-lima <titisau@gmail.com>
2023-08-23 23:57:17 -0300

    chore(request-handler-patch): log warning

--- src/middlewares/requestHandler.ts
+++ src/middlewares/requestHandler.ts
@@ -2,7 +2,12 @@ import { Request, Response } from 'express';
 
 const requestHandler = (req: Request, res: Response) => {
   // se não houver status, a response já foi enviada e não precisa ser enviada uma segunda vez
-  if (!res.locals.status) return;
+  if (!res.locals.status) {
+    console.warn(
+      'Parece que você está usando res.send() ou res.json() diretamente no controller. Isso não vai impedir seu código de funcionar, mas deixa a base de código inconsistente.\nIdealmente, use res.locals em vez disso e chame next().\nUm exemplo pode ser visto em src/controllers/UserController.ts.',
+    );
+    return;
+  }
   res
     .status(res.locals.status)
     .json({ data: res.locals.data, message: res.locals.message });
