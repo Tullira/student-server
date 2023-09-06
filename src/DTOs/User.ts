a24e02457b692f483a122545c85d1c6ca1156f47
titi-lima <titisau@gmail.com>
2023-09-06 18:41:30 -0300

    changed DTO example to partial

--- src/DTOs/User.ts
+++ src/DTOs/User.ts
@@ -1,15 +1,20 @@
 import { z } from 'zod';
 
 export const User = z.object({
-  name: z.string().regex(/^[a-zA-Z\s]+$/, { message: 'O nome deve conter apenas letras' }).nonempty({ message: 'O nome não pode ser vazio' }),
-  phone: z.string().regex(/^\+?[0-9]+$/, { message: 'O número de telefone deve conter apenas números' }).optional(),
+  name: z
+    .string()
+    .regex(/^[a-zA-Z\s]+$/, { message: 'O nome deve conter apenas letras' })
+    .nonempty({ message: 'O nome não pode ser vazio' }),
+  phone: z
+    .string()
+    .regex(/^\+?[0-9]+$/, {
+      message: 'O número de telefone deve conter apenas números',
+    })
+    .optional(),
   email: z.string().email({ message: 'Endereço de email inválido' }),
-  password: z.string().min(8, { message: 'A senha deve ter no mínimo 8 caracteres' }),
+  password: z
+    .string()
+    .min(8, { message: 'A senha deve ter no mínimo 8 caracteres' }),
 });
 
-export const UpdateUser = z.object({
-  name: z.string().regex(/^[a-zA-Z\s]+$/, { message: 'O nome deve conter apenas letras' }).nonempty({ message: 'O nome não pode ser vazio' }).optional(),
-  phone: z.string().regex(/^\+?[0-9]+$/, { message: 'O número de telefone deve conter apenas números' }).optional(),
-  email: z.string().email({ message: 'Endereço de email inválido' }).optional(),
-  password: z.string().min(8, { message: 'A senha deve ter no mínimo 8 caracteres' }).optional(),
-});
+export const UpdateUser = User.partial();
