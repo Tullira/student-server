98b88a1744e7a520f87053aabdc482029e00d80d
damattag <guilherme.damatta@yahoo.com.br>
2024-02-23 13:26:05 -0300

    fix(fix/errorHandler): fixed DTO

--- src/DTOs/User.ts
+++ src/DTOs/User.ts
@@ -2,18 +2,25 @@ import { z } from 'zod';
 
 export const User = z.object({
   name: z
-    .string()
-    .regex(/^[a-zA-Z\s]+$/, { message: 'O nome deve conter apenas letras' })
-    .nonempty({ message: 'O nome não pode ser vazio' }),
+    .string({
+      invalid_type_error: 'O nome deve ser uma string',
+      required_error: 'O nome é obrigatório',
+    })
+    .regex(/^[a-zA-Z\s]+$/, { message: 'O nome deve conter apenas letras' }),
   phone: z
-    .string()
+    .string({ invalid_type_error: 'O número de telefone deve ser uma string' })
     .regex(/^\+?[0-9]+$/, {
       message: 'O número de telefone deve conter apenas números',
     })
     .optional(),
-  email: z.string().email({ message: 'Endereço de email inválido' }),
+  email: z
+    .string({
+      invalid_type_error: 'O email deve ser uma string',
+      required_error: 'O email é obrigatório',
+    })
+    .email({ message: 'Endereço de email inválido' }),
   password: z
-    .string()
+    .string({ invalid_type_error: 'A senha deve ser uma string' })
     .min(8, { message: 'A senha deve ter no mínimo 8 caracteres' }),
 });
 
