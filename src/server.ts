82778690ca3ec060a27c612384cb1a631a389ffd
Tiago Lima <97147969+titi-lima@users.noreply.github.com>
2024-06-13 16:14:27 -0300

    esse erro ficou aqui por tempo demais

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,6 @@ import './env';
 import app from './app';
 import '@database';
 
-app.listen(process.env.SERVER_PORT, () => {
-  console.log(`🚀 Server ready at http://localhost:${process.env.SERVER_PORT}`);
+app.listen(process.env.SERVER_PORT || 3000, () => {
+  console.log(`🚀 Server ready at http://localhost:${process.env.SERVER_PORT || 3000}`);
 });
