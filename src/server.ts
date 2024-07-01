da75fdcf748e04effa29af9ff06f58eb9e99dfdc
Tiago Lima <97147969+titi-lima@users.noreply.github.com>
2024-07-01 15:39:22 -0300

    porta errada.

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,6 @@ import './env';
 import app from './app';
 import '@database';
 
-app.listen(process.env.SERVER_PORT || 3000, () => {
-  console.log(`🚀 Server ready at http://localhost:${process.env.SERVER_PORT || 3000}`);
+app.listen(process.env.SERVER_PORT || 3001, () => {
+  console.log(`🚀 Server ready at http://localhost:${process.env.SERVER_PORT || 3001}`);
 });
